53812a5c489e07dafd64e3ae744fef4da1e9470d
lutzk <lutzk@users.noreply.github.com>
2023-07-19 11:19:31 +0200

    set up eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,41 @@
 module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    // 'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'react',
+    'react-hooks',
+    'react-native',
+    '@react-native',
+    'jest',
+  ],
   root: true,
-  extends: '@react-native',
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    'no-unused-vars': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 1,
+    'no-undef': 'off',
+    'func-call-spacing': 'off',
+    '@typescript-eslint/func-call-spacing': 1,
+    'react-native/no-inline-styles': 0,
+    quotes: [1, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+  },
 };
