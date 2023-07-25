6504478d5c2411805ff0dcbfd958e198af92f3e2
lutzk <lutzk@users.noreply.github.com>
2023-07-25 11:44:39 +0200

    change check for useReactotron

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,7 +1,7 @@
 import { API_BASE_URL, USE_REACTROTRON } from '@env';
 const appConfig = {
   apiBaseUrl: API_BASE_URL,
-  useReactotron: __DEV__ && USE_REACTROTRON !== 'false',
+  useReactotron: __DEV__ && USE_REACTROTRON === 'true',
 } as const;
 
 export { appConfig };
